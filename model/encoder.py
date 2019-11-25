be19d40b3dda24b4317ac44fc89716d926277299
houwenxin <641686859@qq.com>
2019-11-25 23:47:31 +0900

    update

---
+++ model/encoder.py
@@ -0,0 +1,6 @@
+import torch.nn as nn
+
+class Encoder(nn.Module):
+    def __init__(self):
+        super(Encoder, self).__init__()
+        pass
--- trainer.py
+++ trainer.py
@@ -6,3 +6,13 @@
 @LastEditTime: 2019-11-25 20:51:21
 @Description: 
 '''
+
+class Trainer(object):
+    def __init__(self, hps, data_loader):
+        self.hps = hps # Hyper-Parameters
+        self.data_loader = data_loader
+
+        self.build_model()
+    
+    def build_model(self):
+        pass
