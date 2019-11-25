dd2d182b5f28d7350b4491a0aa4a09423f41cf3f
HWX <641686859@qq.com>
2019-11-25 19:03:56 +0900

    Create dataloader.py

---
+++ dataloader.py
@@ -0,0 +1,19 @@
+# -*- coding: UTF-8 -*-
+'''
+@Author: houwx
+@Date: 2019-11-25 19:01:12
+@LastEditors: houwx
+@LastEditTime: 2019-11-25 19:03:45
+@Description: 
+'''
+
+import torch
+
+class Dataset(torch.utils.data.Dataset):
+    def __init__(self):
+        pass
+
+class DataLoader(torch.utils.data.DataLoader):
+    def __init__(self):
+        pass
+    
