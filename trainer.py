697e1cc5cbe13b55aaf0edf09922d5127878d83e
houwenxin <641686859@qq.com>
2019-11-27 00:19:55 +0900

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -7,12 +7,28 @@
 @Description: 
 '''
 
+from torch import optim
+
 class Trainer(object):
-    def __init__(self, hps, data_loader):
+    def __init__(self, hps, data_loader, mode="vqvae"):
         self.hps = hps # Hyper-Parameters
         self.data_loader = data_loader
-
+        self.mode = mode
         self.build_model()
     
     def build_model(self):
-        pass
+        self.encoder_1 = Encoder()
+        self.encoder_2 = Encoder()
+        self.vqlayer = VQLayer()
+        if mode == "vqvae":
+            self.decoder = Decoder()
+            params = list(self.encoder_1.parameters()) + self.encoder_2.parameters()) + self.vqlayer.parameters()) + self.decoder.parameters())
+            self.vqvae_optimizer = optim.Adam(params)
+        elif mode == "gan":
+            self.generator = Generator()
+            self.discriminator = Discriminator()
+            self.gen_optimizer = optim.Adam(self.generator.parameters())
+            self.dis_optimizer = optim.Adam(self.generator.parameters())
+        else:
+            raise NotImplementedError("Invalid Mode!")
+        
