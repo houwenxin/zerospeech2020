c2f893fcde64507249f1f871380a3d10550476e8
HWX <641686859@qq.com>
2019-12-06 09:47:49 +0900

    Try Kaiming Normalization

--- model/modules.py
+++ model/modules.py
@@ -6,10 +6,17 @@ import librosa
 
 def weights_init(m):
     classname = m.__class__.__name__
+    '''
     if classname.find("Conv") != -1:
         m.weight.data.normal_(0.0, 0.02)
     elif classname.find("BatchNorm2d") != -1:
         m.weight.data.normal_(1.0, 0.02)
+        m.bias.data.fill_(0)
+    '''
+    if classname.find("Conv") != -1:
+        nn.init.kaiming_normal_(m.weight)
+    elif classname.find("BatchNorm2d") != -1:
+        nn.init.kaiming_normal_(m.weight)
         m.bias.data.fill_(0)
         
 class Audio2Mel(nn.Module):
